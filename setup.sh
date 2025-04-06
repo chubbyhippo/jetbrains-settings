9c81420a0890fe2ef87bab02be47b5add65a07fd
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-04-06 14:43:36 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@
 
 # intellij ultimate
 curl -s https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/setup-ij.sh | /usr/bin/env sh
+# pycharm
+curl -s https://raw.githubusercontent.com/chubbyhippo/pycharm-settings/refs/heads/main/setup.sh | /usr/bin/env sh
