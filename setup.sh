c9f01a71bc07eabb03430aad6ab0e7a34e3ccf56
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-04-06 14:58:52 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 # intellij ultimate
 curl -s https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/setup-ij.sh | /usr/bin/env sh
+# android studio
+curl -s https://raw.githubusercontent.com/chubbyhippo/android-studio-settings/refs/heads/main/setup.sh | /usr/bin/env sh
 # pycharm
 curl -s https://raw.githubusercontent.com/chubbyhippo/pycharm-settings/refs/heads/main/setup.sh | /usr/bin/env sh
 # webstorm
