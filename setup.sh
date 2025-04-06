71aa8890123922da5ef8a6dfaea5a420559c35f6
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2025-04-06 14:44:23 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@
 curl -s https://raw.githubusercontent.com/chubbyhippo/intellij-settings/refs/heads/main/setup-ij.sh | /usr/bin/env sh
 # pycharm
 curl -s https://raw.githubusercontent.com/chubbyhippo/pycharm-settings/refs/heads/main/setup.sh | /usr/bin/env sh
+# webstorm
+curl -s https://raw.githubusercontent.com/chubbyhippo/webstorm-settings/refs/heads/main/setup.sh | /usr/bin/env sh
